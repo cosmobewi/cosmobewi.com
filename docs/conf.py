df3a35bcb5b549cef1b24d522bcda963cc87ac50
Lionel ORCIL <48251459+ioio2995@users.noreply.github.com>
2025-08-31 11:37:17 +0200

    remove local shared resoures

--- docs/conf.py
+++ docs/conf.py
@@ -1,67 +1,7 @@
-import os
+# docs/conf.py
+from pathlib import Path
 import sys
-from datetime import date
+sys.path.append(str(Path(__file__).resolve().parent / "cosmobewi-shared"))
 
-project = "CosmoBEWI"
-author = "Lionel Orcil — Édité et diffusé par IOBEWI"
-copyright = f"{date.today().year}, {author}"
-
-sys.path.insert(0, os.path.abspath(".."))
-
-extensions = [
-    "sphinx.ext.mathjax",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.todo",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
-    "sphinxcontrib.bibtex",
-]
-
-bibtex_bibfiles = ["references.bib"]
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-    "numpy": ("https://numpy.org/doc/stable/", None),
-}
-
-mathjax3_config = {
-    "tex": {
-        "macros": {
-            r"\dd":  r"{\,\mathrm{d}}",
-            r"\vect": [r"{\boldsymbol{#1}}", 1],
-            r"\avg":  [r"{\langle #1 \rangle}", 1],
-        }
-    }
-}
-
-html_logo = '_static/cosmobewi.jpg'
-html_theme_options = {
-    'logo_only': True,
-}
-
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-html_css_files = ["custom.css"]
-templates_path = ["_templates"]
-html_show_sphinx = False
-html_show_copyright = False
-
-
-source_suffix = ".rst"
-master_doc = "index"
-language = "fr"
-autosummary_generate = True
-nitpicky = False
-
-rst_prolog = r"""
-.. |proj| replace:: Théorie des Horloges Cosmologiques
-.. |LCDM| replace:: :math:`\Lambda\mathrm{CDM}`
-"""
-
-todo_include_todos = True
-
-html_context = {
-    "display_github": True,
-    "github_user": "cosmobewi",
-    "github_repo": "cosmobewi.com",
-    "github_version": "main/docs/",
-}
+from conf_shared import *     # shared theme & config
+include_local_paths(__file__) # enable local overrides
